fac52302b019b891473dddb302b9d7c7a47edf07
Marcelo Teider Lopes <teiderlopes@gmail.com>
2014-01-23 15:04:09 -0200

    Adicionada página de cadastro

---
+++ cadastro.php
@@ -0,0 +1,15 @@
+<html>
+  <body>
+
+    <?php
+
+      $dbconn = pg_connect("host=localhost dbname=teste user=teider password=f3d[]r4!")
+      or die('Não foi possível concectar-se: ' . pg_last_error());
+
+      $resource = pq_query( $dbconn, "SELECT NOME FROM USERS WHERE NOME = " . $_POST["nome"]);
+
+    ?>
+
+  </body>
+</html>
+
