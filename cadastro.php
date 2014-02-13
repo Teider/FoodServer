107c1d245adbdb6e92c8783000942f6739ef94e5
Marcelo Teider Lopes <teiderlopes@gmail.com>
2014-02-13 13:13:53 -0200

    Atualizado. Bla.

--- cadastro.php
+++ cadastro.php
@@ -1,14 +1,16 @@
 <html>
   <body>
 
-    <?php
-
-      $dbconn = pg_connect("host=localhost dbname=teste user=teider password=f3d[]r4!")
-      or die('Não foi possível concectar-se: ' . pg_last_error());
-
-      $resource = pq_query( $dbconn, "SELECT NOME FROM USERS WHERE NOME = " . $_POST["nome"]);
-
-    ?>
+    <form action="verificar_cadastro.php" method="post">
+      <pre>E-mail:           <input type="text" name="email">
+      <pre>Confirmar E-mail: <input type="text" name="verifica_email">
+      <pre>Senha:            <input type="password" name="pwd">
+      <pre>Confirmar Senha:  <input type="password" name="verifica_pwd">
+      <pre>Tipo de conta:    <input type="radio" name="acc_type" value="Admin">Administrador
+      <pre>                  <input type="radio" name="acc_type" value="Restaurante">Restaurante
+      <pre>                  <input type="radio" name="acc_type" value="Usuário">Usuario
+      <input type="submit" value="Cadastrar">
+    </form>
 
   </body>
 </html>
