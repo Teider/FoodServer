09cb3175e1ac0b91862d34756b5d6f60cd374daa
Marcelo Teider Lopes <teiderlopes@gmail.com>
2014-02-13 14:04:43 -0200

    Adicionada validação no form de cadastro. Precisa testar.

--- cadastro.php
+++ cadastro.php
@@ -1,7 +1,46 @@
 <html>
+  <head>
+    <script>
+      function validar_campos() {
+         var temp1 = document.forms["form_cadastro"]["email"].value;
+        if (temp1 = null || temp1 = "") {
+          alert("É necessário um e-mail para realizar o cadastro.");
+          return false;
+        }
+
+        temp1 = document.forms["form_cadastro"]["pwd"].value;
+         if (temp1 = null || temp1 = "") {
+          alert("É necessário uma senha para realizar o cadastro.");
+          return false;
+        }
+
+        if ($('input[type=radio]:checked').size() = 0) {
+          alert("É necessário escolher um tipo de conta para realizar o cadastro");
+          return false;
+        }
+
+        temp1 = document.forms["form_cadastro"]["email"].value;
+        var temp2 = document.forms["form_cadastro"]["verifica_email"].value;
+        if (temp1 != temp2) {
+          alert("Dois endereços de e-mail diferentes foram digitados. Por favor verifique seu e-mail.");
+          return false;
+        }
+
+        temp1 = document.forms["form_cadastro"]["pwd"].value;
+        temp2 = document.forms["form_cadastro"]["verifica_pwd"].value;
+        if (temp1 != temp2) {
+          alert("Duas senhas diferentes foram digitadas. Por favor verifique sua senha.");
+          return false;
+        }
+
+        document.forms["form_cadastro"].submit();
+      }
+      </script>
+    </head>
+      
   <body>
 
-    <form action="verificar_cadastro.php" method="post">
+    <form name="form_cadastro" action="verificar_cadastro.php" method="post">
       <pre>E-mail:           <input type="text" name="email">
       <pre>Confirmar E-mail: <input type="text" name="verifica_email">
       <pre>Senha:            <input type="password" name="pwd">
@@ -9,9 +48,8 @@
       <pre>Tipo de conta:    <input type="radio" name="acc_type" value="Admin">Administrador
       <pre>                  <input type="radio" name="acc_type" value="Restaurante">Restaurante
       <pre>                  <input type="radio" name="acc_type" value="Usuário">Usuario
-      <input type="submit" value="Cadastrar">
+      <button type="button" onclick="validar_campos()">Cadastrar</button>
     </form>
-
   </body>
 </html>
 
