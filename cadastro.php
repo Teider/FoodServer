bf457ad116e9f613fcc018af60108b0bd2640b11
Marcelo Teider Lopes <teiderlopes@gmail.com>
2014-02-13 14:28:04 -0200

    Consertada a validação no cadastro.

--- cadastro.php
+++ cadastro.php
@@ -2,23 +2,27 @@
   <head>
     <script>
       function validar_campos() {
-         var temp1 = document.forms["form_cadastro"]["email"].value;
-        if (temp1 = null || temp1 = "") {
+        var temp1 = document.forms["form_cadastro"]["email"].value;
+        if (temp1 == null || temp1 == "") {
           alert("É necessário um e-mail para realizar o cadastro.");
           return false;
         }
 
         temp1 = document.forms["form_cadastro"]["pwd"].value;
-         if (temp1 = null || temp1 = "") {
+         if (temp1 == null || temp1 == "") {
           alert("É necessário uma senha para realizar o cadastro.");
           return false;
         }
 
-        if ($('input[type=radio]:checked').size() = 0) {
-          alert("É necessário escolher um tipo de conta para realizar o cadastro");
-          return false;
+        if (!document.getElementById("adm").checked) {
+          if (!document.getElementById("rest").checked) {
+            if (!document.getElementById("usr").checked) {
+              alert("É necessário escolher um tipo de conta");
+              return false;
+            }
+          }
         }
-
+        
         temp1 = document.forms["form_cadastro"]["email"].value;
         var temp2 = document.forms["form_cadastro"]["verifica_email"].value;
         if (temp1 != temp2) {
@@ -45,9 +49,9 @@
       <pre>Confirmar E-mail: <input type="text" name="verifica_email">
       <pre>Senha:            <input type="password" name="pwd">
       <pre>Confirmar Senha:  <input type="password" name="verifica_pwd">
-      <pre>Tipo de conta:    <input type="radio" name="acc_type" value="Admin">Administrador
-      <pre>                  <input type="radio" name="acc_type" value="Restaurante">Restaurante
-      <pre>                  <input type="radio" name="acc_type" value="Usuário">Usuario
+      <pre>Tipo de conta:    <input type="radio" name="acc_type" value="Admin" id="adm">Administrador
+      <pre>                  <input type="radio" name="acc_type" value="Restaurante" id="rest">Restaurante
+      <pre>                  <input type="radio" name="acc_type" value="Usuário" id="usr">Usuario
       <button type="button" onclick="validar_campos()">Cadastrar</button>
     </form>
   </body>
